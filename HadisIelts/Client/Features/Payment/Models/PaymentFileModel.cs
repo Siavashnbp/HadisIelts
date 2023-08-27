caae346e29d059b13cef38bf3ea2c8ed0c6b502a
Siavash Nabipour <51988240+Siavashnbp@users.noreply.github.com>
2023-08-28 01:15:17 +0430

    upload payment picture
    1-upload payment page
    2-view images in modal

---
+++ HadisIelts/Client/Features/Payment/Models/PaymentFileModel.cs
@@ -0,0 +1,16 @@
+﻿namespace HadisIelts.Client.Features.Payment.Models
+{
+    public class PaymentFileModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string FileSuffix { get; set; }
+        public string Data { get; set; }
+        public DateTime DateTime { get; set; }
+        public static string ViewImageData(string imageData)
+        {
+            return String.Format("data:image/gif;base64,{0}", imageData);
+        }
+
+    }
+}
